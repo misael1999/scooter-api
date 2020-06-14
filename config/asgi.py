af96ce95e82a43f07dbc9c53fbd63b955e76c9cc
Misael <misael.gonzalez@alumno.uttehuacan.edu.mx>
2020-06-14 17:24:31 +0000

    Modifications in server prod

--- config/asgi.py
+++ config/asgi.py
@@ -9,18 +9,13 @@ from channels.routing import get_default_application
 import sys
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+
 # This allows easy placement of apps within the interior
 # scooter directory.
-app_path = os.path.abspath(os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), os.pardir))
-sys.path.append(os.path.join(app_path, 'scooter'))
-
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.production"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
-
 django.setup()
 application = get_default_application()
