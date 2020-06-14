72b3dd90d6f47e338d4d56876b37f56abb0aef3e
misael <misael.gonzalez@alumno.uttehuacan.edu.mx>
2020-06-14 12:26:03 -0500

    Edit requriments

--- config/asgi.py
+++ config/asgi.py
@@ -20,7 +20,6 @@ sys.path.append(os.path.join(app_path, 'scooter'))
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.production"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
 django.setup()
 application = get_default_application()
