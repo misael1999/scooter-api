aa49b6f934e2f7ca3f5e73d44cc1738597b9d464
misael <misael.gonzalez@alumno.uttehuacan.edu.mx>
2020-07-14 19:46:22 -0500

    Filter by in process

--- scooter/apps/orders/utils/filters.py
+++ scooter/apps/orders/utils/filters.py
@@ -1,14 +1,14 @@
-from django_filters.rest_framework import FilterSet, CharFilter
-
+from django_filters.rest_framework import FilterSet, CharFilter, BooleanFilter
 from scooter.apps.orders.models import Order
 
 
 class OrderFilter(FilterSet):
     order_status = CharFilter(method='filter_by_ids')
+    in_process = BooleanFilter()
 
     class Meta:
         model = Order
-        fields = ['order_status']
+        fields = ['order_status', 'in_process']
 
     def filter_by_ids(self, queryset, name, value):
         order_status_ids = value.strip().split(',')
