0a66f520f737233a0285693563470b1dd6b98481
misael <misael.gonzalez@alumno.uttehuacan.edu.mx>
2020-07-16 01:37:23 -0500

    Add id in serializer station

--- scooter/apps/stations/serializers/stations.py
+++ scooter/apps/stations/serializers/stations.py
@@ -136,7 +136,7 @@ class StationWithAllInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Station
         fields = (
-            'contact_person', 'reputation', 'picture', 'station_name',
+            'id','contact_person', 'reputation', 'picture', 'station_name',
             "user", "services", "schedules", "address",
             "assign_delivery_manually",
             "cancellation_policies",
