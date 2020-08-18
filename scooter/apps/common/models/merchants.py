d4d21275f1eae5393da84de1d3b2921dcffcae81
misael <misael.gonzalez@alumno.uttehuacan.edu.mx>
2020-08-18 00:17:36 -0500

    Change max length path image of category

--- scooter/apps/common/models/merchants.py
+++ scooter/apps/common/models/merchants.py
@@ -9,7 +9,7 @@ class CategoryMerchant(ScooterModel):
 
     name = models.CharField(max_length=70)
     slug_name = models.CharField(max_length=50)
-    image = models.CharField(max_length=50, null=True)
+    image = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return self.name
@@ -19,7 +19,7 @@ class SubcategoryMerchant(ScooterModel):
     name = models.CharField(max_length=70)
     slug_name = models.CharField(max_length=50)
     category = models.ForeignKey(CategoryMerchant, on_delete=models.CASCADE, related_name="subcategories")
-    image = models.CharField(max_length=50, null=True)
+    image = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return self.name
