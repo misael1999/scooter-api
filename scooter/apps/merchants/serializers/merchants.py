f978ee6e1eb7ec73af5d618b442628297d778b52
misael <misael.gonzalez@alumno.uttehuacan.edu.mx>
2020-09-05 00:15:55 -0500

    Add geo field point in merchants

--- scooter/apps/merchants/serializers/merchants.py
+++ scooter/apps/merchants/serializers/merchants.py
@@ -29,9 +29,10 @@ class MerchantWithAllInfoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Merchant
+        geo_field = 'point'
         fields = ('id', 'user', 'contact_person', 'picture', 'merchant_name', 'phone_number', 'is_delivery_by_store',
                   'information_is_complete', 'category', 'subcategory', 'reputation', 'description',
-                  'approximate_preparation_time', 'schedules', 'is_open')
+                  'approximate_preparation_time', 'schedules', 'is_open', 'point')
         read_only_fields = fields
 
 
