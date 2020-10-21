955032c292325da24d3642767d9452446170a413
misael <misael.gonzalez@alumno.uttehuacan.edu.mx>
2020-10-21 00:46:50 -0500

    Fixed get orders summary

--- scooter/apps/merchants/serializers/statistics.py
+++ scooter/apps/merchants/serializers/statistics.py
@@ -2,6 +2,7 @@
 # Django rest framework
 from rest_framework import serializers
 # Models
+from scooter.apps.orders.models import Order
 from scooter.apps.orders.serializers.v2 import OrderWithoutInfoSerializer
 
 
@@ -11,7 +12,7 @@ class SummarySalesSerializer(serializers.Serializer):
         try:
             request = self.context['request']
             month = request.query_params.get('month', 1)
-            orders = merchant.order_set.filter(date_delivered_order__month=month)
+            orders = merchant.order_set.filter(date_delivered_order__month=month, order_status__slug_name="delivered")
             commissions = 0
             if merchant.have_rate:
                 commissions = len(orders) * merchant.rate
