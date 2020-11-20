ffbff405224395560bc4644564449991bfc086df
misael <misael.gonzalez@alumno.uttehuacan.edu.mx>
2020-11-20 00:50:52 -0600

    Modified managers orders

--- scooter/apps/orders/managers/orders.py
+++ scooter/apps/orders/managers/orders.py
@@ -6,4 +6,8 @@ class OrderManager(Manager):
     def get_queryset(self):
         return super(OrderManager, self).get_queryset().select_related(
             'merchant',
+            'station',
+            'customer'
+        ).prefetch_related(
+            'details'
         )
