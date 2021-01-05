82493e0ab523533a169a21b165ebb385110a6499
Misael <misael.gonzalez@alumno.uttehuacan.edu.mx>
2021-01-05 13:55:15 -0600

    Ordering by asc in messages support

--- scooter/apps/support/views/support.py
+++ scooter/apps/support/views/support.py
@@ -1,4 +1,4 @@
-from django_filters import OrderingFilter
+from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
