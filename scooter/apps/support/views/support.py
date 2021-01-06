16013afefd0bfab35555d0ccada17938a72632b4
Misael <misael.gonzalez@alumno.uttehuacan.edu.mx>
2021-01-05 18:34:20 -0600

    Send message data when is created

--- scooter/apps/support/views/support.py
+++ scooter/apps/support/views/support.py
@@ -1,7 +1,8 @@
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import mixins
+from rest_framework import mixins, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from scooter.apps.common.mixins import AddSupportMixin
 from scooter.apps.support.serializers import SupportMessageSimpleSerializer, CreateMessageSupportSerializer
@@ -23,3 +24,13 @@ class SupportMessageViewSet(ScooterViewSet, AddSupportMixin, mixins.ListModelMix
         if self.action == 'create':
             return CreateMessageSupportSerializer
         return self.serializer_class
+
+    def create(self, request, *args, **kwargs):
+        serializer = CreateMessageSupportSerializer(
+            data=request.data,
+            context=self.get_serializer_context()
+        )
+        serializer.is_valid(raise_exception=True)
+        message = serializer.save()
+
+        return Response(data=message, status=status.HTTP_201_CREATED)
