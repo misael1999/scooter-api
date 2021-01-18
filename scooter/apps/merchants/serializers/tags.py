5c5b3f05b3871d11461c02bee0f6ba8d51cc0715
Misael <misael.gonzalez@alumno.uttehuacan.edu.mx>
2021-01-18 17:39:27 -0600

    Add and delete merchant tags

--- scooter/apps/merchants/serializers/tags.py
+++ scooter/apps/merchants/serializers/tags.py
@@ -12,7 +12,9 @@ from scooter.utils.serializers.scooter import ScooterModelSerializer
 
 class TagModelSerializer(ScooterModelSerializer):
 
-    picture = Base64ImageField(allow_null=True, allow_empty_file=True, required=False)
+    picture = Base64ImageField(allow_null=True,
+                               allow_empty_file=True,
+                               required=False)
 
     class Meta:
         model = Tag
@@ -38,6 +40,62 @@ class TagModelSimpleSerializer(ScooterModelSerializer):
         read_only_fields = fields
 
 
+class CreateMerchantTagSerializer(serializers.Serializer):
+    tags = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Tag.objects.filter(status_id=1)
+    )
+    delete_tags = serializers.PrimaryKeyRelatedField(
+        many=True,
+        required=False,
+        queryset=Tag.objects.filter(status_id=1)
+    )
+
+    def create(self, data):
+        try:
+            delete_tags = data.get('delete_tags', [])
+            data['delete_tags'] = delete_tags
+            tags = data['tags']
+            merchant_tags_to_save = []
+            merchant = self.context['merchant']
+            for tag in tags:
+                # Agregar nuevas tags al comercio
+                try:
+                    MerchantTag.objects.get(merchant=merchant, tag_id=tag.id)
+                except MerchantTag.DoesNotExist:
+                    merchant_tags_to_save.append(MerchantTag(merchant=merchant,
+                                                             tag_id=tag.id,
+                                                             tag_name=tag.name))
+
+            for tag in delete_tags:
+                # Eliminar tags
+                try:
+                    merchant_tag = MerchantTag.objects.get(merchant=merchant, tag_id=tag.id)
+                    merchant_tag.delete()
+                except MerchantTag.DoesNotExist:
+                    pass
+            MerchantTag.objects.bulk_create(merchant_tags_to_save)
+            return data
+        except ValueError as e:
+            raise serializers.ValidationError({'detail': str(e)})
+        except Exception as ex:
+            print("Exception in create merchant tag, please check it")
+            print(ex.args.__str__())
+            raise serializers.ValidationError({'detail': 'Error al asignar etiquetas al comercio'})
+
+
+class MerchantTagSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MerchantTag
+        fields = (
+            'id',
+            'tag_id',
+            'merchant',
+            'tag_name'
+        )
+        read_only_fields = fields
+
+
 class MerchantTagModelSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -49,11 +107,11 @@ class MerchantTagModelSerializer(serializers.ModelSerializer):
         try:
             tag = data['tag']
             data["tag_name"] = tag.name
-            return super().create(data)
+            return super(MerchantTagModelSerializer, self).create(data)
         except ValueError as e:
             raise serializers.ValidationError({'detail': str(e)})
         except Exception as ex:
             print("Exception in create product, please check it")
             print(ex.args.__str__())
-            raise serializers.ValidationError({'detail': 'Error registrar el metodo de pago en el comercio'})
+            raise serializers.ValidationError({'detail': 'Error al registrar una nueva etiqueta'})
 
--- scooter/apps/merchants/views/tags.py
+++ scooter/apps/merchants/views/tags.py
@@ -7,60 +7,62 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
+from scooter.apps.common.mixins import AddMerchantMixin
 from scooter.apps.common.models import Status
 from scooter.apps.customers.permissions import IsStation
 from scooter.apps.merchants.models import MerchantTag, Tag
-from scooter.apps.merchants.permissions import IsAccountOwnerMerchant
-# Models
-from scooter.apps.merchants.models.payments import MerchantPaymentMethod
 # Serializers
-from scooter.apps.merchants.serializers import MerchantTagModelSerializer, TagModelSerializer, TagModelSimpleSerializer
-from scooter.apps.merchants.serializers.payments import (MerchantPaymentMethodModelSerializer,
-                                                         MerchantPaymentMethodSimpleSerializer)
+from scooter.apps.merchants.serializers import MerchantTagModelSerializer, TagModelSerializer, TagModelSimpleSerializer, \
+    CreateMerchantTagSerializer, MerchantTagSimpleSerializer
+
 # Utilities
 from scooter.utils.viewsets import ScooterViewSet
 
 
 class MerchantTagViewSet(ScooterViewSet, mixins.ListModelMixin,
                          mixins.RetrieveModelMixin, mixins.CreateModelMixin,
-                         mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+                         mixins.UpdateModelMixin, mixins.DestroyModelMixin, AddMerchantMixin):
     """ Handle add payment methods """
     queryset = MerchantTag.objects.all()
+    permission_classes = (IsAuthenticated, IsStation)
     serializer_class = MerchantTagModelSerializer
-    # filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
-    # search_fields = ('name',)
+    filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
+    search_fields = ('tag_name',)
     # ordering_fields = ('name',)
     # filter_fields = ('status',)
     lookup_field = 'id'
+    merchant = None
 
-    def get_permissions(self):
-        permission_classes = []
-        if self.action in ['create', 'update', 'destroy', 'perform_destroy']:
-            permission_classes = [IsAuthenticated, IsStation]
-        if self.action in ['list']:
-            permission_classes = [AllowAny]
-
-        return [permission() for permission in permission_classes]
+    # def get_permissions(self):
+    #     permission_classes = []
+    #     if self.action in ['create', 'update', 'destroy', 'perform_destroy']:
+    #         permission_classes = [IsAuthenticated, IsStation]
+    #     if self.action in ['list']:
+    #         permission_classes = [AllowAny]
+    #
+    #     return [permission() for permission in permission_classes]
 
     def get_serializer_class(self):
         if self.action == 'list':
-            return MerchantPaymentMethodSimpleSerializer
+            return MerchantTagSimpleSerializer
+        if self.action == 'create':
+            return CreateMerchantTagSerializer
         return self.serializer_class
 
-    def perform_destroy(self, instance):
-        try:
-            sts = Status.objects.get(slug_name='inactive')
-            instance.status = sts
-            instance.save()
-        except Status.DoesNotExist:
-            error = self.set_error_response(status=False, field='status',
-                                            message='Ha ocurrido un error al borrar la etiqueta')
-            return Response(data=error, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    # def perform_destroy(self, instance):
+    #     try:
+    #         sts = Status.objects.get(slug_name='inactive')
+    #         instance.status = sts
+    #         instance.save()
+    #     except Status.DoesNotExist:
+    #         error = self.set_error_response(status=False, field='status',
+    #                                         message='Ha ocurrido un error al borrar la etiqueta')
+    #         return Response(data=error, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class TagViewSet(ScooterViewSet, mixins.ListModelMixin,
                  mixins.RetrieveModelMixin, mixins.CreateModelMixin,
-                 mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+                 mixins.DestroyModelMixin):
 
     queryset = Tag.objects.all()
     serializer_class = TagModelSerializer
